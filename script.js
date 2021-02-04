93e1e2256f20e6e9847783d80f578aac95c4fce3
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-02-04 22:39:13 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $("#changeColor").change(function(){
+        var checked = $(this).prop("checked");
+        $("body").removeClass();
+        if(checked){
+            $("body").addClass("dark");
+        }else {
+            $("body").addClass("neumorphism");
+        }
+    });
+});
